273bc38673ccefcfbc8816614553c2704a748c9e
joerundh <joerundh.p01@gmail.com>
2025-06-15 12:00:39 +0200

    fixed import bug (a stupid one at that), and started building

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/clerk-react";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +24,12 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClerkProvider>
-          <header>
-
+          <header className={"flex flex-row justify-between"}>
+            <button>d</button>
+            <button>e</button>
           </header>
           <main>
-
+            {children}
           </main>
           <footer>
             
