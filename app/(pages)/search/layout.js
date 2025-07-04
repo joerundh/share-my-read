09be583744e8588897c69d84d49512693282f563
joerundh <joerundh.p01@gmail.com>
2025-07-04 12:01:04 +0200

    added suspension

--- app/(pages)/search/layout.js
+++ app/(pages)/search/layout.js
@@ -1,14 +1,20 @@
 import getQueryClient from "@/app/lib/getQueryClient";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 export default async function Layout({ children }) {
     const queryClient = getQueryClient();
 
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
-            <div className={"flex flex-col justify-start gap-3"}>
-                {children}
-            </div>
+            <Suspense fallback={<>
+                    <h2 className={"font-bold text-center text-lg"}>Loading</h2>
+                    <p>Please wait...</p>
+                </>}>
+                <div className={"flex flex-col justify-start gap-3"}>
+                    {children}
+                </div>
+            </Suspense>
         </HydrationBoundary>
     );
 }
